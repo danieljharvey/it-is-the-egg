12b2bbafb3951385f71b65c004fa638e1014c08c
Daniel Harvey <danieljamesharvey@gmail.com>
2017-09-19 00:55:07 +0100

    Prototype board

---
+++ include/ts/Board.ts
@@ -0,0 +1,25 @@
+import { fromJS, List } from "immutable";
+
+// new board is built from JS array
+// all changes reuse the re-generated List object
+
+export class Board {
+  protected list: List;
+
+  constructor(values: array, list: List = null) {
+    if (values) {
+      this.list = fromJS(values); // create new
+    } else {
+      this.list = list; // re-use old
+    }
+  }
+
+  getTile(x: number, y: number) {
+    return this.list.getIn([x, y]);
+  }
+
+  modify(x: number, y: number, tile: any) {
+    const updatedList = this.list.setIn([x, y], tile);
+    return new Board(undefined, updatedList);
+  }
+}
